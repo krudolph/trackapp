b3f306f02d62dab2a0a5fd979397ce857259f19e
Kim Rudolph <kcd.rudolph@gmail.com>
2013-06-26 16:39:57 +0200

    Switch to the rails 4.0.0 release

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
 
 # Make sure the secret is at least 30 characters and all random,
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Trackapp::Application.config.secret_key_base = 'e79786af072c6a3f7b2b9b2397b1fc31c435526a0f59cc0832a37eb6b9a857cdda9594fc60940b47dae1479c4336e3d8b9fc3677c36f42cf047b35ed4b2bcbea'
+Trackapp::Application.config.secret_key_base = 'ea8bd98798f1514cc972b90f4efb1af6e5f38324dd6342938baa1926a65299a5f7f6a8a9796d481a993d2716c2d11e868c28ab954554ee4cce966539acd556db'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Trackapp::Application.config.session_store :encrypted_cookie_store, key: '_trackapp_session'
+Trackapp::Application.config.session_store :cookie_store, key: '_trackapp_session'
